733e3db001895637322d42bfa2245a6e1937993d
Drew Beckmen <drew.beckmen@yale.edu>
2022-02-18 16:47:02 -0500

    adjust node version to comply with build

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,8 +6,11 @@ REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
   --query "Account" --output text`
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
-
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"  # remember that S3 bucket names must be globally unique.
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
 
 # Deploy the S3 bucket (static resources)
 echo -e "\n\n=========== Deploying setup.yml ==========="
@@ -18,7 +21,8 @@ aws cloudformation deploy \
   --template-file setup.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET
+  --parameter-overrides \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
@@ -29,3 +33,9 @@ aws cloudformation deploy \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+  GitHubOwner=$GH_OWNER \
+  GitHubRepo=$GH_REPO \
+  GitHubBranch=$GH_BRANCH \
+  GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
